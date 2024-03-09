2b2bb89268d95561203271694520fda1176ad176
Omar Fetooh <omarfetooh62@gmail.com>
2024-03-09 23:36:35 +0200

    Finish the Menu Page

---
+++ src/home.js
@@ -0,0 +1,10 @@
+function homeContentPage(){
+    const element=document.createElement('div');
+    element.innerHTML=`
+    
+    `
+
+    return element;
+}
+
+export default homeContentPage;
---
+++ src/index.js
@@ -0,0 +1,15 @@
+console.log("ramdan kareem");
+import './style.css';
+import MenuContentPage from './menu';
+import oldPhoto from './photos/oldPhoto.jpg'
+import rockets from './photos/rockets.jpg'
+import kremaAndMksrat from '/src/photos/kremaAndMksrat.jpg'
+
+const container=document.querySelector('#content');
+const navButtons=document.querySelectorAll('nav button');
+
+// navButtons.forEach(btn => {
+//     btn.addEventListener('click',)
+// });
+
+container.appendChild(MenuContentPage());
