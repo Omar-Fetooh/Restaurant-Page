4e6eb1068a3d482837f32d0f203a5188b47f1080
Omar Fetooh <omarfetooh62@gmail.com>
2024-03-10 03:47:14 +0200

    Add the logic of moving between pages

--- src/index.js
+++ src/index.js
@@ -3,10 +3,6 @@ import './style.css';
 import MenuContentPage from './menu';
 import homeContentPage from './home';
 import contactContentPage from './contact';
-import oldPhoto from './photos/oldPhoto.jpg'
-import rockets from './photos/rockets.jpg'
-import kremaAndMksrat from '/src/photos/kremaAndMksrat.jpg'
-
 
 const container=document.querySelector('#content');
 const navButtons=document.querySelectorAll('nav button');
@@ -16,5 +12,28 @@ const navButtons=document.querySelectorAll('nav button');
 // });
 
 // container.appendChild(MenuContentPage());
-// container.appendChild(homeContentPage())
-container.appendChild(contactContentPage());
+container.appendChild(homeContentPage())
+// container.appendChild(contactContentPage());
+
+for(let i=0;i<navButtons.length;i++){
+   navButtons[i].addEventListener('click',()=>{
+    container.innerHTML="";
+    navButtons[0].classList.remove('active');
+    navButtons[1].classList.remove('active');
+    navButtons[2].classList.remove('active');
+    if(navButtons[i].textContent=="Home"){
+        container.appendChild(homeContentPage())
+        navButtons[i].classList.add('active');
+    }
+    else if(navButtons[i].textContent=="Menu"){
+         container.appendChild(MenuContentPage());
+        navButtons[i].classList.add('active');
+
+    }
+    else if(navButtons[i].textContent=="Contact"){
+         container.appendChild(contactContentPage());
+        navButtons[i].classList.add('active');
+
+    }
+   })
+}
